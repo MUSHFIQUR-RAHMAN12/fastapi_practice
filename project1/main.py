96d60ae1014d97ddd03d1720e19a460d293f9805
Mushfiqur Rahman <mushfiqrandom12@gmail.com>
2026-05-09 23:03:58 +0600

    delete and path query parameters learned

--- project1/main.py
+++ project1/main.py
@@ -4,9 +4,9 @@ app= FastAPI()
 
 todos= [{"title": "task1", "description": "this is task1", "iscomplete": False},
         
-        {"title": "task2", "description": "this is task2","iscomplete": False},
+        {"title": "task2", "description": "this is task2","iscomplete": True},
         {"title": "task3", "description": "this is task3","iscomplete": False},
-        {"title": "task4", "description": "this is task4","iscomplete": False}]
+        {"title": "task4", "description": "this is task4","iscomplete": True}]
 
 @app.get("")
 def hello_world():
@@ -27,5 +27,23 @@ def update_todo(todo_id: int, updated_todo=Body()):
     if 0 <= todo_id < len(todos):
         todos[todo_id] = updated_todo
         return {"message": "Todo updated successfully", "todo": updated_todo}
+    else:
+        return {"message": "Todo not found"}, 404
+    
+
+
+@app.delete("/todos/delete/{todo_id}")
+def delete_todo(todo_id: int):
+    if 0 <= todo_id < len(todos):
+        deleted_todo = todos.pop(todo_id)
+        return {"message": "Todo deleted successfully", "todo": deleted_todo}
+    else:
+        return {"message": "Todo not found"}, 404
+    
+
+@app.get("/todos/iscomplete/")
+def is_todo_complete(todo_id: int):
+    if 0 <= todo_id < len(todos):
+        return todos[todo_id]["iscomplete"]
     else:
         return {"message": "Todo not found"}, 404
